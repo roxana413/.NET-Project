8068d9842c72f557c963660292b83277e25b03a4
roxana413 <73295914+roxana413@users.noreply.github.com>
2021-11-04 21:37:02 +0200

    Initial commit

---
+++ Back-end/PricePrediction/PricePrediction/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PricePrediction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
