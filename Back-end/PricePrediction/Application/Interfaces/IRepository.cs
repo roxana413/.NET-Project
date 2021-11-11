e364f24f7a2545f3f3f4fd71b01970a59431c0ba
bulbucrazvan <rbulbuc1@gmail.com>
2021-11-11 17:53:34 +0200

    .

---
+++ Back-end/PricePrediction/Application/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : BaseEntity
+    {
+        Task<TEntity> AddAsync(TEntity entity);
+        Task<TEntity> UpdateAsync(TEntity entity);
+        Task<TEntity> DeleteAsync(TEntity entity);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity> GetByIdAsync(Guid id);
+    }
+}
+
